0358197271407e8d779d40f858c5bca29bda6101
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2020-10-21 23:54:52 -0500

    WIP

--- cl.exs
+++ cl.exs
@@ -4,6 +4,6 @@ predicted = [ 5.0, 0.0, 12.0]
 real
 |> Enum.zip(predicted)
 |> Enum.map(fn {x, y} -> :math.pow(x-y, 2) end)
-|> Enum.sum()
-|> Kernel./(Enum.count(real))
-|> IO.puts
+|> Enum.sum
+# |> Kernel./(Enum.count(real))
+# |> &(IO.puts(&1/Enum.count(real)))
